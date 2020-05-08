ae5b8aa2c108242f8d5e6ad9ac468bda28164e96
Diego Sanchez <diegosandmg@gmail.com>
2020-05-08 17:32:21 -0400

    Build prepare

---
+++ precache-manifest.afcaa856b6789f6b9abffe0c1582e1f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d2491eb8bacdef8b09c6a53b54888b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "833060a0f0827018fd5a",
+    "url": "/static/css/main.657be06b.chunk.css"
+  },
+  {
+    "revision": "684a0afaa4774aa7e4a5",
+    "url": "/static/js/2.d2621481.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d2621481.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "833060a0f0827018fd5a",
+    "url": "/static/js/main.aa527350.chunk.js"
+  },
+  {
+    "revision": "d4fa88bbdd6e2d736214",
+    "url": "/static/js/runtime-main.74039080.js"
+  }
+]);
