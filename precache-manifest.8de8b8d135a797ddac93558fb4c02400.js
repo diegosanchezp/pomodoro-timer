a3f0139000ff94e1c3566068f45a1a503859a2a6
Diego Sanchez <diegosandmg@gmail.com>
2020-05-08 17:54:50 -0400

    Updates

---
+++ precache-manifest.8de8b8d135a797ddac93558fb4c02400.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "613db851e395ae09ec8126532943ac74",
+    "url": "/diegosanchezp/pomodoro-timer/index.html"
+  },
+  {
+    "revision": "c2b79ba1216e54978c3c",
+    "url": "/diegosanchezp/pomodoro-timer/static/css/main.657be06b.chunk.css"
+  },
+  {
+    "revision": "684a0afaa4774aa7e4a5",
+    "url": "/diegosanchezp/pomodoro-timer/static/js/2.d2621481.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/diegosanchezp/pomodoro-timer/static/js/2.d2621481.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2b79ba1216e54978c3c",
+    "url": "/diegosanchezp/pomodoro-timer/static/js/main.d29f1a16.chunk.js"
+  },
+  {
+    "revision": "c9cf11d9748bdb65f902",
+    "url": "/diegosanchezp/pomodoro-timer/static/js/runtime-main.a092fba0.js"
+  }
+]);
